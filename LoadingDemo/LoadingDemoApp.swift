f2926630b2a62aeab8e12bbbee06dee81dfe7680
Sheikh Bayazid <sheikhbayazid@gmail.com>
2021-03-27 10:57:29 +0600

    Initial Commit

---
+++ LoadingDemo/LoadingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoadingDemoApp.swift
+//  LoadingDemo
+//
+//  Created by Sheikh Bayazid on 3/27/21.
+//
+
+import SwiftUI
+
+@main
+struct LoadingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
