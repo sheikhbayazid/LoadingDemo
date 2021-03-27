88c18847b34216f4ed88325af898bd73ede8b0f4
Sheikh Bayazid <sheikhbayazid@gmail.com>
2021-03-27 13:02:06 +0600

    Initial Commit

--- LoadingDemo/ContentView.swift
+++ LoadingDemo/ContentView.swift
@@ -8,9 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isLoading = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ZStack {
+                //Color.yellow.ignoresSafeArea()
+                
+                VStack {
+                    Text("Content")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .padding()
+                        .shadow(radius: 10)
+                }.frame(width: 350, height: 500)
+                .background(Color.yellow)
+                .clipShape(RoundedRectangle(cornerRadius: 20), style: FillStyle())
+                .shadow(radius: 10)
+                
+                
+                if isLoading {
+                    ZStack {
+                        Color.white.opacity(0.5).ignoresSafeArea()
+                        
+                        VStack(spacing: 5) {
+                            ProgressView()
+                                .progressViewStyle(CircularProgressViewStyle(tint: .black))
+                            Text("Loading...")
+                                .font(.footnote)
+                                .foregroundColor(.black)
+                        }
+                    }//.frame(width: 100, height: 55)
+                    //.clipShape(RoundedRectangle(cornerRadius: 10))
+                }
+                
+            }.onAppear(perform: starFakeNNetworkCall)
+            .navigationTitle("Loading Test")
+        }
+    }
+    
+    func starFakeNNetworkCall() {
+        self.isLoading = true
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            withAnimation(.easeInOut(duration: 0.25)) {
+                self.isLoading = false
+            }
+            
+        }
     }
 }
 
